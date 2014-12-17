2bbceaa723c486e3a6778cf3754fb1b0ddde8349
Irvel Nduva <irvelnduva@hotmail.com>
2014-12-16 21:15:47 -0600

    Funcionalidad "tecnicamente" terminada
    Ya está casi completa la funcionalidad de la página.
    
    Falta corregir el método de ordenamiento
    Falta agregar el texto de "Numero de calificaciones"
    Falta "embellecer" el texto del resultado

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var indice = 0;
 var profesores = {
-	nombres: ["Juan Antonio Alanís Rodríguez", 
+	nombres: [
+	"Juan Antonio Alanís Rodríguez", 
 	"Guillermina Jauregui Arroyo",
 	"Isabel Cristina Elizondo Ordóñez",
 	"Raúl Flores Herrera",
@@ -18,19 +19,66 @@ var profesores = {
 	"Andrés Guerrero Elizondo",
 	"María Guadalupe Tobías Lara"
 	],
-	nocalificaciones: [49,40,56,57,116,56,78,59,35,4,70,11,44,11,2,57,29],
+	numcalificaciones: [49,40,56,57,116,56,78,59,35,4,70,11,44,11,2,57,29],
 	facilidad: [6.9,7.8,5.5,5.4,8.3,5.7,4.9,5.8,3.8,10.0,6.5,6.3,7.4,6.8,6.0,7.2,6.6],
 	ayuda: [6.2,6.2,5.5,6.0,9.4,5.2,4.4,7.3,8.7,8.5,7.3,5.1,7.6,5.7,8.5,8.0,6.9],
-	claridad: [6.0,6.5,6.2,5.9,9.6,4.8,5.1,7.2,8.5,8.5,8.0,6.3,7.8,6.4,5.0,7.9,4.9]
+	claridad: [6.0,6.5,6.2,5.9,9.6,4.8,5.1,7.2,8.5,8.5,8.0,6.3,7.8,6.4,5.0,7.9,4.9],
+	//Inicializar el ranking con valores cualquiera (Los de claridad)
+	ranking:[6.0,6.5,6.2,5.9,9.6,4.8,5.1,7.2,8.5,8.5,8.0,6.3,7.8,6.4,5.0,7.9,4.9],
+	prioridad1: 1,
+	prioridad2: 1,
+	prioridad3: 1,
+	calcularRanking : function() {
+		console.log("Sin ordenar: ");
+		for(var contador = 0; contador < this.nombres.length; contador++){
+			this.ranking[contador] = (this.facilidad[contador]*this.prioridad1) + (this.ayuda[contador]*this.prioridad2) + (this.claridad[contador]*this.prioridad3);
+			console.log(this.ranking[contador]);
+		}
+    },
+    ordenarRanking : function() {
+    	//Lo se, es bubble sort :/ pero son sólo 17! YOLO
+    	var intercambiados;
+    	do {
+        	intercambiados = false;
+        	for (var i = 0; i < this.ranking.length-1; i++) {
+            	if (this.ranking[i] > this.ranking[i+1]) {
+                	var temp1 = this.ranking[i];
+                	var temp2 = this.nombres[i];
+                	var temp3 = this.numcalificaciones[i];
+                	var temp4 = this.facilidad[i];
+                	var temp5 = this.ayuda[i];
+                	var temp6 = this.claridad[i];
+                	this.ranking[i] = this.ranking[i+1];
+                	this.nombres[i] = this.nombres[i+1];
+                	this.numcalificaciones[i] = this.numcalificaciones[i+1];
+                	this.facilidad[i] = this.facilidad[i+1];
+                	this.ayuda[i] = this.ayuda[i+1];
+                	this.claridad[i] = this.claridad[i+1];
+                	this.ranking[i+1] = temp1;
+                	this.nombres[i+1] = temp2;
+                	this.numcalificaciones[i+1] = temp3;
+                	this.facilidad[i+1] = temp4;
+                	this.ayuda[i+1] = temp5;
+                	this.claridad[i+1] = temp6;
+                	this.intercambiados = true;
+            	}
+        	}
+    	} while (intercambiados);
+    	console.log("ordenados: ");    
+		for(var contador = 0; contador < this.nombres.length; contador++){
+			console.log(this.ranking[contador]);
+		}
+    }
 };
 
 
 
 
 function mejorProfe(){
-	var primeraCualidad = null;
-	var segundaCualidad = null;
-	var terceraraCualidad = null;
+	var primeraCualidad = 0;
+	var segundaCualidad = 0;
+	var terceraCualidad = 0;
+	//Recoger los valores de las opciones seleccionadas
 	var elementos = document.getElementsByClassName("primera-cualidad");
 	for(var i = 1; i < elementos.length; i++){
       if(elementos[i].checked){
@@ -52,23 +100,78 @@ function mejorProfe(){
            i = elementos.length;
       }
 	}
+	primeraCualidad++;
+	segundaCualidad++;
+	terceraCualidad++;
+	var ofacilidad = 1;
+	var oayuda = 1;
+	var oclaridad = 1;
 	/*
-		3 = Facilidad
-		2 = Ayuda
-		1 = Claridad
+		Codigos de los valores regresados por los checkboxes
+		4 = Facilidad
+		3 = Ayuda
+		2 = Claridad
+		1 = Sin preferencia
 	*/
-	var ranking = 0;
+	if(primeraCualidad == 4){
+		ofacilidad = 4;
+		if(segundaCualidad == 3){
+			oayuda = 3;
+			if(terceraCualidad == 2){
+				oclaridad == 2;
+			}
+		}
+		else if(segundaCualidad == 2){
+			oclaridad = 3;
+			if(terceraCualidad == 3){
+				oayuda == 2;
+			}
+		}
+	}
+	else if(primeraCualidad == 3){
+		var oayuda = 4;
+		if(segundaCualidad == 4){
+			ofacilidad = 3;
+			if(terceraCualidad == 2){
+				oclaridad == 2;
+			}
+		}
+		else if(segundaCualidad == 2){
+			oclaridad = 3;
+			if(terceraCualidad == 4){
+				ofacilidad == 2;
+			}
+		}
+	}
+	else if(primeraCualidad == 2){
+		var oclaridad = 4;
+		if(segundaCualidad == 3){
+			oayuda = 3;
+			if(terceraCualidad == 4){
+				ofacilidad == 2;
+			}
+		}
+		else if(segundaCualidad == 4){
+			ofacilidad = 3;
+			if(terceraCualidad == 3){
+				oayuda == 2;
+			}
+		}
+	}
+	//Inicializar objeto
+	profesores.prioridad1 = ofacilidad;
+	profesores.prioridad2 = oayuda;
+	profesores.prioridad3 = oclaridad;
+	profesores.calcularRanking();
+	profesores.ordenarRanking();
 	//var element = document.getElementById("result").value;
 	//element.innerHTML = "New Header";
-	//var indice = 
-	document.getElementById("result").innerHTML = primeraCualidad;
+	document.getElementById("rnombre").innerHTML = profesores.nombres[0];
+	document.getElementById("rfacilidad").innerHTML = profesores.facilidad[0];
+	document.getElementById("rayuda").innerHTML = profesores.ayuda[0];
+	document.getElementById("rclaridad").innerHTML = profesores.claridad[0];
 };
 
-
-/*Evitar dos opciones iguales*/
-function ningunaSelec(nombre){
-
-};
 
 
 function revisarUno1(){
